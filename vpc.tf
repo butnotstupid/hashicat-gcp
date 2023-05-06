b50596800526792f96d1e352e0143f8270d6bd6e
root <butnotstupid@gmail.com>
2023-05-06 20:42:39 +0000

    Fix vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,14 +1,14 @@
-module "network_vpc" {
-    source  = "app.terraform.io/some-name/network/google//modules/vpc"
-    version = "7.0.0"
-    # insert required variables here
-    project_id   = var.project
-    network_name = "gaurav-network"
-    subnets = [
-  {
-    subnet_name   = "gaurav-subnet"
-    subnet_ip     = "10.100.10.0/24"
-    subnet_region = var.region
-  }
-]
+module "network" {
+  source  = "app.terraform.io/some-name/network/google"
+  version = "3.4.0"
+
+  network_name = "gaurav-network"
+  project_id = "var.project"
+  subnets = [   
+        {     
+            subnet_name   = "gaurav-subnet"     
+            subnet_ip     = "10.100.10.0/24"     
+            subnet_region = var.region   
+        } 
+  ]
 }
