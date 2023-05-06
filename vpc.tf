a8a87a7e5aa06790f4611b25593e2a7e29aa72cd
root <butnotstupid@gmail.com>
2023-05-06 20:35:02 +0000

    Fix vpc module

--- vpc.tf
+++ vpc.tf
@@ -4,5 +4,11 @@ module "network_vpc" {
     # insert required variables here
     project_id   = var.project
     network_name = "gaurav-network"
-
+    subnets = [
+  {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
 }
