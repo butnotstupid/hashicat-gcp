3a58bfe09bae229d44013497ba91a9cb648e1707
root <butnotstupid@gmail.com>
2023-05-06 20:36:30 +0000

    Fix vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 module "network_vpc" {
     source  = "app.terraform.io/some-name/network/google//modules/vpc"
-    version = "3.4.0"
+    version = "7.0.0"
     # insert required variables here
     project_id   = var.project
     network_name = "gaurav-network"
