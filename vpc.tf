16fe78dea0a83d95a1b433c8d6d55f51ec050c8c
root <butnotstupid@gmail.com>
2023-05-06 20:28:12 +0000

    Fix vpc module

--- vpc.tf
+++ vpc.tf
@@ -5,11 +5,4 @@ module "network_vpc" {
     project_id   = var.project
     network_name = "gaurav-network"
 
-    subnets = [
-        {
-            subnet_name   = "gaurav-subnet"
-            subnet_ip     = "10.100.10.0/24"
-            subnet_region = var.region
-        }
-    ]
 }
